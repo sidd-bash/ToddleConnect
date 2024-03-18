a868305962dc0171769d6860d9c83a9594312dd9
sidd-bash <77910988+sidd-bash@users.noreply.github.com>
2024-03-18 17:09:53 +0530

    .vercel removed

--- server/.vercel/functions/cors.js
+++
@@ -1,8 +0,0 @@
-// .vercel/functions/cors.js
-export default async function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (adjust for security if needed)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Methods allowed by Socket.IO
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Headers allowed by Socket.IO
-    res.status(200).json({ message: 'CORS enabled' });
-  }
-  
